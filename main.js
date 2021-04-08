86901b495f0961df00c7421e0a57020f3f121512
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-04-07 20:47:30 -0700

    ac

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require("electron");
 function createWindow() {
 	console.log("hello");
 	// Create the browser window.
-	const win = new BrowserWindow({ width: 800, height: 600 });
+	const win = new BrowserWindow({ width: 1280, height: 720});
 
 	// and load the html of the app.
 	win.loadFile("./camera.html");
